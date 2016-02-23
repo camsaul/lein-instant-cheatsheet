0798823f7b17da612c5cc6dca1fadc8c582c7e08
Cam Saül <cammsaul@gmail.com>
2016-02-23 14:36:56 -0800

    Bump deps

--- project.clj
+++ project.clj
@@ -1,16 +1,16 @@
-(defproject lein-instant-cheatsheet "2.1.4"
+(defproject lein-instant-cheatsheet "2.1.5"
   :description "Leiningen plugin to create a cheatsheet for your Clojure project <3"
   :url "https://github.com/cammsaul/lein-instant-cheatsheet"
   :license {:name "The MIT License"
             :url "http://opensource.org/licenses/MIT"}
   :min-lein-version "2.0.0"
   :eval-in-leiningen true
-  :dependencies [[org.clojure/java.classpath "0.2.2"]
+  :dependencies [[org.clojure/java.classpath "0.2.3"]
                  [org.clojure/tools.namespace "0.2.10"]
                  [amalloy/ring-gzip-middleware "0.1.3"]
-                 [cheshire "5.4.0"]
-                 [compojure "1.3.3"]
+                 [cheshire "5.5.0"]
+                 [compojure "1.4.0"]
                  [hiccup "1.0.5"]
-                 [ring/ring-jetty-adapter "1.3.2"]]
-  :profiles {:dev {:plugins [[jonase/eastwood "0.2.1"]
-                             [lein-bikeshed "0.2.0"]]}})
+                 [ring/ring-jetty-adapter "1.4.0"]]
+  :profiles {:dev {:plugins [[jonase/eastwood "0.2.3"]
+                             [lein-bikeshed "0.3.0"]]}})
